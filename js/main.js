d7b494ef8dde534e0ea0ead81d8d6a64ab01de89
Valeev Bulat <cirkach95@gmail.com>
2016-05-18 17:07:18 +0300

    some fix

--- js/main.js
+++ js/main.js
@@ -14,6 +14,18 @@ function clickVideo() {
         videoContainer.style.display = "none";
     }
 }
+function clickVideo2() {
+    var videoContainer = document.getElementById("s-video-window2");
+    var bodyR = $("#body");
+
+    if(!bodyR.hasClass("no-scroll")){
+        bodyR.addClass("no-scroll");
+        videoContainer.style.display = "block";
+    }else{
+        bodyR.removeClass("no-scroll");
+        videoContainer.style.display = "none";
+    }
+}
 $('.s-services').on('click', '.s-services__nav.s-services__arrow', function(){
     $('.s-services__nav.s-nav-top__link--active').removeClass("s-nav-top__link--active").addClass("s-services__arrow");
     $('.s-services__page.s-services__page--active').removeClass("s-services__page--active");
--- js/translate.js
+++ js/translate.js
@@ -12,6 +12,7 @@
     $.i18n.init(options, function() {
         $('body').i18n();
         $('#' + $.i18n.lng() + '_lang').addClass('s-nav-top__link--active');
+        $('.s-services__video').css('background-image', 'url(./images/primer.svg)');
     });
 
     window.changeLang = function(lang) {
@@ -19,6 +20,16 @@
         $.i18n.setLng(lang);
         $('body').i18n();
         $('#' + $.i18n.lng() + '_lang').addClass('s-nav-top__link--active');
+
+        if ($('.s-services__video').css('background-image') == 'url(./images/primer.svg)')
+            $('.s-services__video').css('background-image', 'url(./images/primer2.svg)');
+        else $('.s-services__video').css('background-image', 'url(./images/primer.svg)');
     };
 
+    $('#en_lang').click(function(){
+        $('.s-services__video').css('background-image', 'url(./images/primer2.svg)');
+    });
+    $('#ru_lang').click(function(){
+        $('.s-services__video').css('background-image', 'url(./images/primer.svg)');
+    });
 })();
