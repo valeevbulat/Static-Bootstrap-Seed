9076c9d7897db3f952004944cdd65c7d200110c5
Valeev Bulat <cirkach95@gmail.com>
2016-05-31 22:43:19 +0300

    some fix

--- js/translate.js
+++ js/translate.js
@@ -13,6 +13,8 @@
         $('body').i18n();
         $('#' + $.i18n.lng() + '_lang').addClass('s-nav-top__link--active');
         $('.s-services__video').css('background-image', 'url(./images/primer.svg)');
+        document.title = document.getElementById('title').textContent;
+
         if($.cookie('lang') == 'en'){
             changeLang('en');
             $('.s-services__video').css('background-image', 'url(./images/primer2.svg)');
@@ -29,7 +31,13 @@
         if ($('.s-services__video').css('background-image') == 'url(./images/primer.svg)')
             $('.s-services__video').css('background-image', 'url(./images/primer2.svg)');
         else $('.s-services__video').css('background-image', 'url(./images/primer.svg)');
+
+        other_function();
     };
+
+    function other_function() {
+        document.title = document.getElementById('title').textContent;
+    }
 
     $('#en_lang').click(function(){
         $('.s-services__video').css('background-image', 'url(./images/primer2.svg)');
